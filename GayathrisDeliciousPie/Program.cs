0a164c986d632df0bd7bc4f800401a5bead68779
gayatpr15 <gayatpr@gmail.com>
2025-04-17 16:42:44 -0400

    Added Azure Key Vault for a on Premisis DB connection as secret

--- GayathrisDeliciousPie/Program.cs
+++ GayathrisDeliciousPie/Program.cs
@@ -1,5 +1,9 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using GayathrisDeliciousPie.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +11,23 @@ builder.Services.AddScoped<IPieRepository, PieRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 builder.Services.AddScoped<IShoppingCart, ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+
+if (builder.Environment.IsProduction()) {
+    var keyVaultURL = builder.Configuration.GetSection("KeyVault:url");
+    var keyClientId = builder.Configuration.GetSection("KeyVault:clientId");
+    var keyClientSecret = builder.Configuration.GetSection("KeyVault:clientSecret");
+    var keyDirectoryId = builder.Configuration.GetSection("KeyVault:directoryId");
+
+    var credential = new ClientSecretCredential(keyDirectoryId.Value!.ToString(), keyClientId.Value!.ToString(),keyClientSecret.Value!.ToString());
+    builder.Configuration.AddAzureKeyVault(keyVaultURL.Value!.ToString(), keyClientId.Value!.ToString(), keyClientSecret.Value!.ToString(), new DefaultKeyVaultSecretManager());
+    var client = new SecretClient(new Uri(keyVaultURL.Value!.ToString()), credential);
+    
+    builder.Services.AddDbContext<GayathrisPieShopDbContext>(options =>
+    {
+        //options.UseSqlServer(builder.Configuration["ConnectionStrings:GayathrisPieShopDbContextConnection1"]);
+        options.UseSqlServer(client.GetSecret("ProdConnectionDB").Value.Value.ToString());
+    });
+}
 
 
 builder.Services.AddSession();
@@ -19,7 +40,7 @@ builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 builder.Services.AddDbContext<GayathrisPieShopDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration["ConnectionStrings:GayathrisPieShopDbContextConnection"]);
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:GayathrisPieShopDbContextConnection1"]);
 });
 
 var app = builder.Build();
